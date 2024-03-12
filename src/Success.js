27e1562b9d97c948d0a2178b71790e5bfcbbb568
StyledCactus <xavier.work06@gmail.com>
2024-03-12 11:06:40 -0400

    Sending for review. Collect data then come back and update.

--- src/Success.js
+++ src/Success.js
@@ -1,12 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import MonikaAproved from "./images/MonikaAproved.png"
 
-
 function Success(){
+
+    const Navigate = useNavigate();
+
+    const toStart = () => {
+        Navigate('/');
+    }
+
     return(
         <div className="sucbox">
             <h1 className="resSuc">Table reserved successfully!</h1>
             <img src={MonikaAproved} className="MonAProved"/>
-            <button className="byor">BUY MORE</button>
+            <button className="byor" onClick={toStart}>BUY MORE</button>
         </div>
 )
 }
